7f288273166873402bdb40756e3bf1fe854cca15
Usama Razzaq <usama.razzaq@usti.com.pk>
2023-10-30 10:42:09 +0500

    Description of your changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:easyrishta/View/ChatScreen/chatroom.dart';
-import 'package:easyrishta/View/ChatScreen/voice_msg.dart';
 import 'package:easyrishta/View/Dashboard/dashboard_screen.dart';
 import 'package:easyrishta/View/Dashboard/make_match.dart';
 import 'package:easyrishta/View/Dashboard/notification_screen.dart';
@@ -47,7 +46,7 @@ class MyApp extends StatelessWidget {
           return GetMaterialApp(
               debugShowCheckedModeBanner: false,
               theme: kAppThemeData[AppTheme.light],
-              initialRoute: 'chatroom',
+              initialRoute: 'splash',
               routes: {
                 'splash': (context) => const Splashscreen(),
                 'phone': (context) => const MyPhone(),
@@ -66,7 +65,6 @@ class MyApp extends StatelessWidget {
                 'allprofile': (context) => const OtherProfile(),
                 'matching': (context) => const MatchesProfile(),
                 'chatroom': (context) => ChatScreen(),
-                // 'chatroom': (context) => const VoiceChat(),
                 'forgetpassword': (context) => const ForgetPassword(),
                 'notification': (context) => const NotificationScreen(),
               });
